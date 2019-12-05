7e5bb5d94ce133366454580dd081dadf96e60b46
kredmond <kelsu02@gmail.com>
2019-12-05 13:37:13 -0700

    Finished - had to google the math

--- answer.js
+++ answer.js
@@ -1,1 +1,10 @@
 // *** YOUR ANSWER BELOW ***
+function doMath(int) {
+    for (let i = 1; i <= int; i++) {
+        if ((i % 6 === 0 || i % 7 === 0) !== (i % 6 === 0 && i % 7 === 0)) {
+            console.log(i);
+        }
+    }
+}
+
+doMath(100);
